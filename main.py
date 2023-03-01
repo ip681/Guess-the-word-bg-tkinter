14a9d21f778499225d340dd173c4cfbdab77b0e5
Ivo Petkov <48838737+ip681@users.noreply.github.com>
2023-03-01 23:47:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,154 @@ MAX_TURNS = 12
 # Създаваме графичното приложение
 root = tk.Tk()
 root.title("Познай думата")
-root.geometry("600x400")
+root.geometry("400x300")
 
 # Определяме променливи за думата, познатите букви и грешните опити
 word = random.choice(["куче", "котка", "мечка", "лисица", "лимон", "банан"])
 guesses = set()
 wrong_guesses = set()
-wrong_guesses_counter = 0
+wrong_attempts = 0
+wrong_attempts_words = ["""
+________     
+|            
+|            
+|            
+|            
+|            
+|            
+|____________
+""",
+"""
+________     
+|      |     
+|            
+|            
+|            
+|            
+|            
+|____________
+""",
+"""
+________     
+|      |     
+|      O     
+|            
+|            
+|            
+|            
+|____________
+""",
+"""
+________     
+|      |     
+|      O     
+|      |     
+|            
+|            
+|            
+|____________
+""",
+"""
+________     
+|      |     
+|      O     
+|      |     
+|      |     
+|            
+|            
+|____________
+""",
+"""
+________     
+|      |     
+|      O     
+|     /|     
+|      |     
+|            
+|            
+|____________
+""",
+"""
+________     
+|      |     
+|      O     
+|     /|     
+|    / |     
+|            
+|            
+|____________
+""",
+"""
+________     
+|      |     
+|      O     
+|     /|\    
+|    / |     
+|            
+|            
+|____________
+""",
+"""
+________     
+|      |     
+|      O     
+|     /|\    
+|    / | \   
+|            
+|            
+|____________
+""",
+"""
+________     
+|      |     
+|      O     
+|     /|\    
+|    / | \   
+|     /      
+|            
+|____________
+""",
+"""
+________     
+|      |     
+|      O     
+|     /|\    
+|    / | \   
+|     /      
+|    /       
+|____________
+""",
+"""
+________     
+|      |     
+|      O     
+|     /|\    
+|    / | \   
+|     / \    
+|    /       
+|____________
+""",
+"""
+________     
+|      |     
+|      O     
+|     /|\    
+|    / | \   
+|     / \    
+|    /   \   
+|____________
+"""
+]
+
+# Определяме функция, която връща думата за броя на грешните опити
+def get_wrong_attempts_word():
+    return wrong_attempts_words[wrong_attempts]
 
 
 # Дефинираме функцията, която ще бъде извиквана при натискане на буква
 def guess_letter(letter):
     if letter in word:
+        global wrong_attempts
         guesses.add(letter)
         update_display()
         if all(letter in guesses for letter in word):
@@ -26,8 +162,9 @@ def guess_letter(letter):
             # root.quit() # Не затваряме играта веднага след победа
     else:
         wrong_guesses.add(letter)
+        wrong_attempts += 1  # увеличаваме брояча на грешните опити
         update_display()
-        if len(wrong_guesses) >= MAX_TURNS:
+        if wrong_attempts >= MAX_TURNS:
             show_message(f"Загубихте! Думата беше '{word}'.")
             # root.quit()  # Не затваряме играта веднага след загуба
 
@@ -37,12 +174,22 @@ def update_display():
     display_word = " ".join([letter if letter in guesses else "_" for letter in word])
     display_word_label.config(text=display_word)
     wrong_guesses_label.config(text="Грешни опити: " + ", ".join(sorted(wrong_guesses)))
+    wrong_attempts_label.config(text=f"Брой грешни опити: {wrong_attempts}")  # обновяваме брояча на грешните опити
+    wrong_attempts_label.config(text=f"{get_wrong_attempts_word()}")
 
 
 # Дефинираме функцията, която показва съобщение на екрана
 def show_message(message):
     message_label.config(text=message)
 
+
+# def show_message(wrong_attempts_f):
+#     if wrong_attempts == 0:
+#         message_label.config(text="Започваме играта. Познай думата!")
+#     elif wrong_attempts < MAX_TURNS:
+#         message_label.config(text=f"Опит {wrong_attempts} от {MAX_TURNS}. Опитай отново!")
+#     else:
+#         message_label.config(text=f"Загубихте! Думата беше '{word}'.")
 
 
 # Добавяме елементи към графичното приложение
@@ -63,15 +210,11 @@ wrong_guesses_label.pack()
 message_label = tk.Label(root, text="")
 message_label.pack()
 
-text_label = tk.Label(root, text="""________    
-|      |     
-|      O     
-|     /|\\    
-|    / | \\   
-|     / \\    
-|    /   \\   
-|____________""", font=('Consolas', 15))
-text_label.pack()
+# wrong_attempts_label = tk.Label(root, text="Брой грешни опити: 0")
+# wrong_attempts_label.pack()
+
+wrong_attempts_label = tk.Label(root, text=f"{get_wrong_attempts_word()}", font=('Consolas', 15))
+wrong_attempts_label.pack()
 
 # Стартираме графичното приложение
 root.mainloop()
